9c57052fe8f871c925378d8bd7c0049a3df53eb5
Noah Gibbs <noah@ooyala.com>
2013-02-04 11:17:31 -0800

    Add expand_path workaround and comment to explain it.

--- lib/jruby-msgpack.rb
+++ lib/jruby-msgpack.rb
@@ -1,6 +1,9 @@
 require "java"
 
-require File.join(File.dirname(__FILE__), "..", "ext", "javassist.jar")
-require File.join(File.dirname(__FILE__), "..", "ext", "msgpack-0.6.6-SNAPSHOT.jar")
+# Workaround: JRuby doesn't handle ".." in "jar:file:" paths.  File.expand_path
+# removes them.
+ext_dir = File.expand_path(File.join(File.dirname(__FILE__), "..", "ext"))
+require File.join(ext_dir, "javassist.jar")
+require File.join(ext_dir, "msgpack-0.6.6-SNAPSHOT.jar")
 
 require File.join(File.dirname(__FILE__), "jruby-msgpack", "unpacker")
